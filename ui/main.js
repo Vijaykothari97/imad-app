aa777de67108724635b9728904b478ef3ade0a95
Vijaykothari97 <36456740+Vijaykothari97@users.noreply.github.com>
2018-03-12 23:05:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,9 @@ console.log('Loaded!');
  var element = document.getElementById("he");
  element.innerHTML="CAN I SAY";
      
-    var element= document.getElementById("hello");
+    var button= document.getElementById("hello");
 button.onclick = function (){
    
- element.innerHTML="holla";
+ button.innerHTML="holla";
 };
      

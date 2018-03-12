f149beae66fe02ca0a4c0ddfd7096fe73f3f3dfd
Vijaykothari97 <36456740+Vijaykothari97@users.noreply.github.com>
2018-03-13 00:13:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,32 @@ console.log('Loaded!');
  var element = document.getElementById("he");
  element.innerHTML="CAN I SAY";
      
-    var button= document.getElementById("hello");
+      var button= document.getElementById("hello");
 button.onclick = function (){
    
 vj1.innerHTML="holla";
 };
+    
+    
+submit.onclick=function(){
+    
+
+    
+var http=new XMLHttpRequest();
+    request.onreadystatechange = function(){
+    // Process the server response here.
+if(request.readystate===DONE){
+    
+
+if(request.status===200){
      
+    
+}
+}
+        
+    };
+var name=document.getElementById("MAN");
+
+    request.open('GET', 'http://www.example.org/some.file', true);
+request.send(null);
+};

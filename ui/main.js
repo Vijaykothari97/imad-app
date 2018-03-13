5075eb0a40e12b9a638331eb4bc9ad4f985b79fe
Vijaykothari97 <36456740+Vijaykothari97@users.noreply.github.com>
2018-03-14 02:18:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,6 +32,6 @@ if(request.readystate===XMLHttpRequest.DONE){
 };
 var nameInput=document.getElementById("name");
 var name=nameInput.value;
-request.open('GET',' http://vijaykothari97.imad.hasura-app.io/submit-name', true);
+request.open('GET',' http://vijaykothari97.imad.hasura-app.io/submit-name?name='+name, true);
 request.send(null);
 };

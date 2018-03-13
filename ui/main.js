e4dba9e62c974aee2b5723105ab3fb4ecd53edf6
Vijaykothari97 <36456740+Vijaykothari97@users.noreply.github.com>
2018-03-14 02:24:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,13 +17,13 @@ if(request.readystate===XMLHttpRequest.DONE){
      names=JSON.parse(names);
    
    var list='';
-   for(var i=0;i<names.length;i++){
-        list+=names[i];
+   for(var i=0; i < names.length;i++){
+        list+='<li>'+names[i]+'</li>',;
         
         
     }
-    var element= document.getElementById("demo");
-   element.innerHTML=list;
+    var ul= document.getElementById("demo");
+   ul.innerHTML=list;
     
      }
 

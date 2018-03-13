32603f62db889686375a751c6e055c9834c49704
Vijaykothari97 <36456740+Vijaykothari97@users.noreply.github.com>
2018-03-14 00:18:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/submit-name', function (req, res) {
    var name=req.query.name;
    
-   res.send(JSON.strigify(name));
+   res.send(name);
 });
   
  app.get('/ui/main.js', function (req, res) {

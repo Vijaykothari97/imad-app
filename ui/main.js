4a61fc9f57721dbdddded56c4fc46e749c38710e
Vijaykothari97 <36456740+Vijaykothari97@users.noreply.github.com>
2018-04-23 02:41:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -66,7 +66,7 @@ xhttp.send();
         
    if (names[i] ==='animal'){
     var hy =   document.getElementById("demo1");
-   hy.innerHTML=names[i].x;    
+   hy.innerHTML="hey";    
    
        
    }     

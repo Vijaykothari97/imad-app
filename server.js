a69888a91bb580f9ccf7c6684166945217649901
Vijaykothari97 <36456740+Vijaykothari97@users.noreply.github.com>
2018-04-23 01:54:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
-
+var names=[];
 app.get('/:name', function (req, res) {
   var name=req.params.name;
   res.send(names[name]);

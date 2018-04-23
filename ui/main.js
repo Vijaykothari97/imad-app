44070ea68795afa3eca1835864bcad203839793e
Vijaykothari97 <36456740+Vijaykothari97@users.noreply.github.com>
2018-04-23 15:13:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,11 +22,11 @@
    for(var i=0 ; i < names.length ; i++) {
         list+=names[i];
         
-   if (list ==='*'){
+   if (elements[i] ===''){
     var f =   document.getElementById("demo");
    f.innerHTML=null;
-   }
   
+    }
    }
          
        

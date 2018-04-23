0dcce79bb157be430b2b799015124903cf3e3da1
Vijaykothari97 <36456740+Vijaykothari97@users.noreply.github.com>
2018-04-23 14:43:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,12 +55,7 @@ app.get('/counter', function (req, res) {
 
 
  
- var names=[];
-app.get('/:name', function (req, res) {
-  var name=req.params.name;
- names.push(name);
-  res.send(JSON.stringify(name));
-}); 
+ 
  
 app.get('/article', function (req, res) {
    res.send(createTemplate(article));

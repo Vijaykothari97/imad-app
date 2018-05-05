fb85c96c0668f7f9f199e471dc4999faa1d62c71
Vijaykothari97 <36456740+Vijaykothari97@users.noreply.github.com>
2018-05-05 15:59:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -59,8 +59,28 @@ xhttp.send();
     var x=['lion','tiger','chetah','elephant','wolf','leopard','hyena','bear','deer','hippo'];
     var y=['parrot','sparrow','pigeon','crow','peacock','koyal','woodpecker'];
     var shirts=['10 shirts of pantaloon','10 shirts of zamkuddi','10shirts from wholesale company'];
-    var items=['samosa'];
+    
    
+var button=document.getElementById("s2");
+button.onclick=function(){
+var s3 = parseInt(prompt("Enter the amount"))  ; 
+var samosa= s3;
+var c = document.getElementById("s4");
+c.innerHTML="the price of samosa is "+samosa+"<br/>";
+  
+  var button=document.getElementById("v2");
+  button.onclick=function(){
+   var vadaPav = parseInt(prompt("Enter the amount"));
+   var f = document.getElementById("v4");
+   f.innerHTML="the price of vadapav is "+vadaPav+"<br/>";
+   var total=samosa+ vadaPav;
+   var c = document.getElementById("t2");
+   c.innerHTML="The total bill amount is "+total;
+    };
+    
+};  
+  
+  
    for(var i=0 ; i < elements.length ; i++) {
         
         
@@ -105,24 +125,7 @@ xhttp.send();
 };
 
 
-var button=document.getElementById("s2");
-button.onclick=function(){
-var s3 = parseInt(prompt("Enter the amount"))  ; 
-var samosa= s3;
-var c = document.getElementById("s4");
-c.innerHTML="the price of samosa is "+samosa+"<br/>";
-  
-  var button=document.getElementById("v2");
-  button.onclick=function(){
-   var vadaPav = parseInt(prompt("Enter the amount"));
-   var f = document.getElementById("v4");
-   f.innerHTML="the price of vadapav is "+vadaPav+"<br/>";
-   var total=samosa+ vadaPav;
-   var c = document.getElementById("t2");
-   c.innerHTML="The total bill amount is "+total;
-    };
-    
-};
+
 
 
 
